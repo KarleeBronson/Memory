c5322c29b46c55d7d571fff4afc0dced347bf39a
KarleeBronson <kbronson@192.168.1.179>
2013-09-27 18:00:39 -0600

    Project 1

---
+++ Project01/src/project01/Project01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project01;
+
+/**
+ *
+ * @author kbronson
+ */
+public class Project01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
