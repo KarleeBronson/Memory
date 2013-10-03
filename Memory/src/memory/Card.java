84f59e269907d3100bb63feffc70eca6a4c05423
karfos18 <fos10001@byui.edu>
2013-10-03 13:19:34 -0700

    Changes and additions made by Karalee Olsen

---
+++ Memory/src/memory/Card.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memory;
+
+/**
+ *
+ * @author Karalee Olsen
+ */
+public class Card {
+   /* - value - a variable for the word, number, or picture on the card 
+    */
+    
+    Star star;
+    
+    
+    public Card(){
+        
+    }
+    
+    public void cardColor(Star, star){
+        this.star = star;
+}
---
+++ Memory/src/memory/Pair.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memory;
+
+/**
+ *
+ * @author Karalee Olsen
+ */
+public class Pair {
+    /* - card1 - a variable of type Card that holds a reference to the first card in the pair
+-       card2 - a variable of type Card that holds a reference to the second card in the pair
+*   */
+    
+    String cardType
+    Card card1;
+    Card card2;
+    Card card3;
+    Card card4;
+    Card card5;
+    Card card6;
+    Card card7;
+    Card card8;
+    Card card9;
+    Card card10;
+    
+    
+    public Pair(){
+        
+    }
+    
+    public void numberOfPairs(){
+        
+    }
+}
