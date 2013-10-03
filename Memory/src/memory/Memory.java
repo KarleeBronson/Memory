dd6ba6297aeff3ed4c2ea8efc05fc9efa96916c2
KarleeBronson <kbronson@192.168.1.179>
2013-10-03 08:42:27 -0600

    Project 1- First assignment

--- Memory/src/memory/Memory.java
+++ Memory/src/memory/Memory.java
@@ -4,16 +4,40 @@
  */
 package memory;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kbronson
  */
 public class Memory {
-
+    String name; 
+    String instructions = "This is the game of Memory \n\n"
+            + "There will be a total of 2 players.\n\n"
+            + "Each player will take a turn choosing 1 card at a time.\n\n"
+            + "By clicking on a card with the arrow, card will flip over.\n\n"  
+            + "The object of the game is to find the cards match.\n\n"
+            + "The player continues until they can't find a match.\n\n"
+            + "The player who gets the most matches wins.\n\n";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Memory myGame = new Memory();
+        myGame.getName();
+        myGame.displayHelp();
         // TODO code application logic here
     }
-}
+    public void getName() {
+        Scanner input = new Scanner (System.in);
+        System.out.println ("Enter your name:");
+        this.name = input.next ();
+    }
+    
+    public void displayHelp (){
+    System.out.println("Welcome" + this.name + "");    
+    System.out.println(this.instructions);
+    
+    }}
+            
+      
---
+++ Memory/src/memory/Players.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memory;
+
+/**
+ *
+ * @author kbronson
+ */
+public class Players {
+    lastName,
+    firstName;
+    
+    public static void main(String[] args) {
+               
+    }
+}
+dataType variableName= values
+String lastName
+String firstName
