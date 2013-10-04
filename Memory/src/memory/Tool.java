c39e0105705561f564b651b47d1d0a4781fa7ccd
KarleeBronson <kbronson@192.168.1.179>
2013-10-04 15:53:41 -0600

    Project 1- First assignment

--- Memory/src/memory/Tool.java
+++ Memory/src/memory/Tool.java
@@ -9,11 +9,17 @@ import java.io.Serializable;
  * @author kbronson
  */
 public class Tool implements Serializable{
-
+        //arrowTool=;
+    String arrowTool = "arrow";
     public Tool() {
     }
     
-    
-    
+    public String getArrowTool () {
+            return arrowTool;
+    }
+    public void setArrowTool (string arrowTool){
+        this.arrowTool = arrowTool;
+    }
+    public void displayTool();
+        System.out.println("Tool arrow:" + this.arrowTool);
 }
-
