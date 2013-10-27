78a359a4526985d159e80b5bfc003242de5ae0fd
karfos18 <fos10001@byui.edu>
2013-10-26 18:18:21 -0700

    my individual assignment

--- Memory/src/memory/Board.java
+++ Memory/src/memory/Board.java
@@ -1,7 +1,9 @@
 package memory;
 
 import java.io.Serializable;
+import java.util.Scanner;
 import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Karalee Olsen
@@ -9,13 +11,12 @@ import javax.swing.table.AbstractTableModel;
 public class Board implements Serializable {
 
     char[] cardArray = new char[];       //a two-dimensional array of cards (i.e., the cards laid out face down)
-    //String whoseTurn = "";     //a variable containing a reference to the Player who's turn is next
+    String whoseTurn = "";     //a variable containing a reference to the Player who's turn is next
     int rowCount;
     int columnCount = 5;
     //Location[][] boardLocations;
 
-       public Board(){
-        
+    public Board() {
     }
 
     /*  
@@ -31,12 +32,12 @@ public class Board implements Serializable {
      System.out.print("\n");
        
      }*/
-       
-       // displays whose turn it is.
+    // displays whose turn it is.
     public void displayWhoseTurn() {
         System.out.println("\n It is " + this.whoseTurn + " turn to find a match.");
     }
-        // displays the size of the board
+    // displays the size of the board
+
     public void displaySize() {
         System.out.println("\nThe board is " + this.rowCount + " by "
                 + this.columnCount + " in size.");
@@ -50,16 +51,16 @@ public class Board implements Serializable {
         this.cardArray = cardArray;
     }
 
-   /* public String getWhoseTurn() {
-        
+    public String getWhoseTurn() {
+
         return whoseTurn;
     }
 
     public void setWhoseTurn(String whoseTurn) {
-        
+
         this.whoseTurn = whoseTurn;
     }
-*/
+
     public int getRowCount() {
         rowCount = 5;
         return rowCount;
@@ -79,12 +80,78 @@ public class Board implements Serializable {
     }
 
     /*public Location[][] getBoardLocations() {
-        return boardLocations;
+     return boardLocations;
+     }
+
+     public void setBoardLocations(Location[][] boardLocations) {
+     this.boardLocations = boardLocations;
+     }
+     */
+    private final static String[][] cardMatches = { //this section will list the matches 
+    };
+
+    // displays the help menu
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < Board.cardMatches.length; i++) {
+            System.out.println("\t   " + cardMatches[i][0] + "\t" + cardMatches[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
     }
 
-    public void setBoardLocations(Location[][] boardLocations) {
-        this.boardLocations = boardLocations;
+    /**
+     *
+     * @return
+     */
+    public String collectCards() {
+
+        String gameStatus = Game.PLAYING;
+
+        this.display();
+        // get commands entered
+        String command = this.getCommand();
+
+        int matched;
+        for (matched = 0; matched >= Board.cardMatches.length; matched++) {
+            if ("CC".equals(command)) {
+                System.out.println("\t   A match has been added to your pile. ");
+            }
+        }
+        return null;
+
     }
-    */
-    
+
+    protected final String getCommand() {
+
+        Scanner inFile = Memory.getInputFile(); // create like in the tictactoe example on the main class
+        String command;
+        boolean valid = false;
+        do {
+
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            valid = validCommand(command);
+            if (!validCommand(command)) {
+                new MemoryError().displayError("Invalid command. Please enter a valid command.");
+                continue;
+            }
+
+        } while (!valid);
+
+        return command;
+    }
+
+    // determines if the command is valid
+    private boolean validCommand(String command) {
+        String[][] items = Board.cardMatches;
+
+        for (String[] item : Board.cardMatches) {
+            if (item[0].equals(command)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
